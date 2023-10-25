caedec8e4e396832ad284610e0e0b0fb2709d6cf
Raymond <raymond.mwaiki@student.moringaschool.com>
2023-10-25 19:05:56 +0300

    Completed

---
+++ README.md
@@ -0,0 +1,26 @@
+# FLATDANGO MOVIE TICKETS WEBSITE
+# INTRODUCTION
+* This project was to create a website that sells movie tickets 
+
+# Steps used used to achieve this from the javascript file
+* First I used a function to upDATE movie details on the page 
+* Then I enabled or disabled the buy ticket button depending on if the tickets were available for a particular movie or not
+* I then fetched and displayed details from the first movie when the page loads
+* Fetch the list of movies and create menu items
+* Added an Event listener for clicking on a movie in the menu
+* Added an event listener for buying a ticket
+* I then sent a requestto the server to update the available tickets count
+
+# The link to the project webpage
+* http://127.0.0.1:5500/index.html
+
+# Technology used
+   *Javascript
+   *Git
+   *json server
+
+# Licence
+*Mit license
+
+# Author info
+[Raymond Mwaiki]
--- script.js
+++ script.js
@@ -1,67 +1,73 @@
-// handling ticket purchases
-updateAvailableTickets();
-
-buyButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        // Simulate a ticket purchase by sending a request to a server
-        fetch('/purchaseTicket', { method: 'POST' })
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.success) {
-                    // Update the available tickets count and notify the user
-                    availableTickets -= 1;
-                    updateAvailableTickets();
-                    alert('Ticket purchased! Enjoy the event.');
-                } else {
-                    alert('Ticket purchase failed. Please try again.');
-                }
-            });
-    });
-});
-
-// Example JavaScript for handling ticket purchases and updating available tickets from a JSON file
-const buyButtons = document.querySelectorAll('.buy-button');
-const availableTicketsElement = document.getElementById('available-tickets');
-
-let availableTickets;
-
-// Function to update the available tickets count on the web page
-function updateAvailableTickets() {
-    availableTicketsElement.innerText = `Available Tickets: ${availableTickets}`;
-}
-
-// Function to fetch data from the JSON file
-function fetchData() {
-    fetch('db.json')
-        .then((response) => response.json())
-        .then((data) => {
-            // Assuming the JSON structure has a 'films' array with ticket-related data
-            // You can modify this to match your actual JSON structure
-            const films = data.films;
-            // Calculate available tickets by subtracting the sold tickets from capacity
-            availableTickets = films.reduce((total, film) => total + (film.capacity - film.tickets_sold), 0);
-            // Update the available tickets count on the web page
-            updateAvailableTickets();
+document.addEventListener("DOMContentLoaded", () => {
+    const moviePoster = document.getElementById("movie-poster");
+    const movieTitle = document.getElementById("movie-title");
+    const movieRuntime = document.getElementById("movie-runtime");
+    const movieShowtime = document.getElementById("movie-showtime");
+    const availableTickets = document.getElementById("available-tickets");
+    const buyTicketButton = document.getElementById("buy-ticket");
+    const filmsList = document.getElementById("films");
+  
+    // Function to update movie details on the page
+    function updateMovieDetails(movie) {
+      moviePoster.src = movie.poster;
+      movieTitle.textContent = movie.title;
+      movieRuntime.textContent = movie.runtime + " minutes";
+      movieShowtime.textContent = movie.showtime;
+      const ticketsAvailable = movie.capacity - movie.tickets_sold;
+      availableTickets.textContent = ticketsAvailable;
+  
+      // Enable or disable the "Buy Ticket" button based on whether the tickets are available or not
+      buyTicketButton.disabled = ticketsAvailable === 0;
+    }
+  
+    // Fetch and display the details of the first movie when the page loads
+    fetch("http://localhost:3000/films/1")
+      .then((response) => response.json())
+      .then((data) => {
+        const film = data;
+        updateMovieDetails(film);
+      })
+      .catch((error) => console.error("Error:", error));
+  
+    // Fetch the list of movies and create menu items
+    fetch("http://localhost:3000/films")
+      .then((response) => response.json())
+      .then((data) => {
+        data.forEach((movie) => {
+          const filmItem = document.createElement("li");
+          filmItem.textContent = movie.title;
+          filmItem.classList.add("film", "item");
+          filmItem.setAttribute("data-movie-id", movie.id);
+          filmsList.appendChild(filmItem);
+  
+          // Event listener for clicking on a movie in the menu
+          filmItem.addEventListener("click", () => {
+            fetch(`http://localhost:3000/films/${movie.id}`)
+              .then((response) => response.json())
+              .then((data) => {
+                updateMovieDetails(data);
+              });
+          });
         });
-}
-
-// Call the fetchData function to load data from the JSON file
-fetchData();
-
-buyButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        // Simulate a ticket purchase by sending a request to a server
-        fetch('/purchaseTicket', { method: 'POST' })
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.success) {
-                    // Update the available tickets count and notify the user
-                    availableTickets -= 1;
-                    updateAvailableTickets();
-                    alert('Ticket purchased! Enjoy the event.');
-                } else {
-                    alert('Ticket purchase failed. Please try again.');
-                }
-            });
+      });
+  
+    // Event listener for buying a ticket
+    buyTicketButton.addEventListener("click", () => {
+      const currentTickets = parseInt(availableTickets.textContent, 10);
+      if (currentTickets > 0) {
+        const newTicketsSold = currentTickets - 1;
+        availableTickets.textContent = newTicketsSold;
+        // Send a request to the server to update the available tickets count
+        fetch(`http://localhost:3000/films/1/purchase`, {
+          method: "PATCH",
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("Ticket purchased successfully");
+          })
+          .catch((error) => {
+            console.error("Error purchasing ticket:", error);
+          });
+      }
     });
-});
+  });
